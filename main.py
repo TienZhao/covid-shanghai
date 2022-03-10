657d3cf00c9759d23dee724fb5b8c7b341073b78
Tien <tienzhao@qq.com>
2022-03-11 00:56:56 +0800

    Create method to update demo.html

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import re
+# -*- coding: utf-8 -*-
 
+# Read start_date.txt to end_date-1.txt
 dateArr = range(20220306, 20220311)
 addCount = 0
 addArr = []
+
+# Fix wrong plot locations by replacing the keyword
+fixDict = {
+    '浦东新区竹园小学（长岛校区）': '浦东新区竹园小学（沪东校区）',
+    '闵行区知新村': '闵行区浦江镇知新村',
+}
+
+# Parse addresses from txt
 for date in dateArr:
     path = str(date) + '.txt'
 
@@ -15,8 +25,26 @@ for date in dateArr:
         for addStr in matchArr:
             addCount += 1
             print('<li>' + str(addCount) + '. ' + addStr + '</li>')
+            if addStr in fixDict:
+                addStr = fixDict[addStr]
             if addStr not in addArr:
                 addArr.append(addStr)
+
+# Update demo.html with new address array
+with open('demo.html', 'r+', encoding='utf-8') as f:
+    lines = []
+    for line in f.readlines():
+        startIndex = line.find('var adds = ')
+        if startIndex >= 0:
+            # print(line)
+            lines.append(' '* startIndex + 'var adds = ' + str(addArr) + '\n')
+        else:
+            lines.append(line)
+    f.close()
+
+with open('demo.html', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+    f.close()
 
 print(addArr)
 print("共处理" + str(addCount) + "条地址；去重后共计" + str(len(addArr)) + "个地点。")
