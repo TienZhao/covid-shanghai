2b4f069741dda44be85821380ceb0a1932a1d0e8
Tien <tienzhao@qq.com>
2022-03-19 09:31:03 +0800

    Fix wrong address bug in Minhang

--- geo.py
+++ geo.py
@@ -2,7 +2,7 @@ import json, requests, time
 # -*- coding: utf-8 -*-
 
 geoUrl = 'https://restapi.amap.com/v3/geocode/geo'
-key = '8ce21affa63959ac7e9cec9757bf2fa6'
+key = 'YOUR_AMAP_KEY'
 cityCode = '021'
 
 def getGeo(add):
