a241be9e045ed135c99283600606cba40847624c
Tien <tienzhao@qq.com>
2022-03-22 09:24:40 +0800

    Fix bug

---
+++ text.py
@@ -0,0 +1,19 @@
+import re
+path = 'C:\\HelloWorld\\covid-shanghai\\20220321.txt'
+district = ""
+with open(path, 'r+', encoding='utf-8') as f:
+
+    for line in f.readlines():
+        matchArr = []
+        if len(line) < 6:
+            matchArr = re.findall(r'(.*?)区', line, re.M)
+
+        if len(matchArr) > 0:
+            district = matchArr[0]
+        else:
+            if len(line) > 2 and len(line) < 15:
+                new_line = '居住于' + district + '区' + line
+                print(new_line)
+
+
+    f.close()
