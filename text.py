16b403c235ffc87af6c7718aab9f809abb6d8c0d
Tien <tienzhao@qq.com>
2022-03-23 09:07:39 +0800

    Fix bug

--- text.py
+++ text.py
@@ -1,5 +1,5 @@
 import re
-path = 'C:\\HelloWorld\\covid-shanghai\\20220321.txt'
+path = 'C:\\HelloWorld\\covid-shanghai\\20220322.txt'
 district = ""
 with open(path, 'r+', encoding='utf-8') as f:
 
