7454063063785a2cb679b97acfd909f9c1bdc451
Tien <tienzhao@qq.com>
2022-04-10 14:50:40 +0800

    Update 20220409 data

--- text.py
+++ text.py
@@ -1,5 +1,5 @@
 import re
-path = 'C:\\HelloWorld\\covid-shanghai\\20220408.txt'
+path = 'C:\\HelloWorld\\covid-shanghai\\20220409.txt'
 district = ""
 with open(path, 'r+', encoding='utf-8') as f:
 
